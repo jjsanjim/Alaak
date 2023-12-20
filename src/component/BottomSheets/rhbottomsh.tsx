a2a436866215308a83029b0f80ef950ee44cd56a
jjsanjim <138830212+jjsanjim@users.noreply.github.com>
2023-12-20 12:15:24 -0600

    changes in box structure

--- src/component/BottomSheets/rhbottomsh.tsx
+++ src/component/BottomSheets/rhbottomsh.tsx
@@ -7,67 +7,57 @@ import CheckBox from "@react-native-community/checkbox";
 import { useState } from "react";
 import Calendar from "../Icons/Calendar";
 import Documents from "../Icons/Documents";
+import Copy from "../Icons/Copy";
+import MapIcon from "../Icons/Map";
 
 
 function RhSheet(){
+    const information=[
+        {
+            name:"Fecha de Ingreso",
+            content:"04 de Septiembre de 2023"
+        },
+        {
+            name:"RFC",
+            content:"NATA9602"
+        },
+        {
+            name:"CURP",
+            content:"NATA960202MDFLML04"
+        },
+        {
+            name:"Dirección",
+            content:"AV. Del Imán, Tlalpan CDMX"
+        },
+        {
+            name:"NSS",
+            content:"54781254NSS"
+        },
+        {
+            name:"CV",
+            content:"..."
+        }
+    ]
     return(
         <View style={styles.squareShape}>
                 <Text style={styles.textNames}>Información Personal </Text>
                 <View>
-                    <View style={styles.littleSquare}>
-                        <View style={styles.icons}>
-                            <Calendar/>
-                        </View>
-                        <View style={styles.textShape}>
-                            <Text style={styles.textBox}> Fecha de Ingreso</Text>                    
-                            <Text style={styles.textBox}> 04 de Septiembre de 2023</Text>
-                        </View>
-                    </View>
-                    <View style={styles.littleSquare}>
-                        <View style={styles.icons}>
-                            <Documents/>
-                        </View>
-                        <View style={styles.textShape}>
-                            <Text style={styles.textBox}> RFC</Text>
-                            <Text style={styles.textBox}> NATA9602</Text>                        
-                        </View>
-                    </View>
-                    <View style={styles.littleSquare}>
-                        <View style={styles.icons}>
-                            <Documents/>
-                        </View>
-                        <View style={styles.textShape}>
-                            <Text style={styles.textBox}> CURP</Text>                    
-                            <Text style={styles.textBox}> NATA960202MDFLML04</Text>
-                        </View>
-                    </View>
-                    <View style={styles.littleSquare}>
-                        <View style={styles.icons}>
-                            <Documents/>
-                        </View>
-                        <View style={styles.textShape}>
-                            <Text style={styles.textBox}> Dirección</Text>                    
-                            <Text style={styles.textBox}> AV. Del Imán, Tlalpan CDMX</Text>
-                        </View>
-                    </View>
-                    <View style={styles.littleSquare}>
-                        <View style={styles.icons}>
-                            <Documents/>
-                        </View>
-                        <View style={styles.textShape}>
-                            <Text style={styles.textBox}> NSS</Text>                    
-                            <Text style={styles.textBox}> 54781254NSS</Text>
-                        </View>
-                    </View>
-                    <View style={styles.littleSquare}>
-                        <View style={styles.icons}>
-                            <Documents/>
-                        </View>
-                        <View style={styles.textShape}>
-                            <Text style={styles.textBox}> CV</Text>                    
-                            <Text style={styles.textBox}> ...</Text>
-                        </View>
-                    </View>
+                        {information.map((item,index)=>{
+                                return(
+                                    <View style={styles.littleSquare}>
+                                        <View style={{flexDirection:"row"}}>
+                                            <View style={styles.iconsbs}>
+                                                <MapIcon/>
+                                            </View>
+                                            <View style={styles.textShape}>
+                                                <Text style={styles.textBox}>{item.name}</Text>                    
+                                                <Text style={styles.textBoxtwo}>{item.content}</Text>
+                                            </View>
+                                        </View>
+                                        <Copy/>
+                                    </View>
+                                )
+                            })}    
                 </View>
             </View>
         
---
+++ src/component/Icons/Map.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Svg, Path } from "react-native-svg";
+
+const MapIcon =()=>{
+    return(
+    <Svg width="25" height="25" viewBox="0 0 25 25" fill="none">
+    <Path id="Vector" d="M22.5 9.50002V15.5C22.5 18 22 19.75 20.88 20.88L14.5 14.5L22.23 6.77002C22.41 7.56002 22.5 8.46002 22.5 9.50002Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <Path id="Vector_2" d="M22.23 6.77L6.76999 22.23C3.75999 21.54 2.5 19.46 2.5 15.5V9.5C2.5 4.5 4.5 2.5 9.5 2.5H15.5C19.46 2.5 21.54 3.76 22.23 6.77Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <Path id="Vector_3" d="M20.88 20.88C19.75 22 18 22.5 15.5 22.5H9.50003C8.46003 22.5 7.56002 22.41 6.77002 22.23L14.5 14.5L20.88 20.88Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <Path id="Vector_4" d="M6.74002 8.47997C7.42002 5.54997 11.82 5.54997 12.5 8.47997C12.89 10.2 11.81 11.66 10.86 12.56C10.17 13.22 9.08003 13.22 8.38003 12.56C7.43003 11.66 6.34002 10.2 6.74002 8.47997Z" stroke="white" stroke-width="1.5"/>
+    <Path id="Vector_5" d="M9.5946 9.19995H9.60359" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </Svg>
+    );
+}
+export default MapIcon;
