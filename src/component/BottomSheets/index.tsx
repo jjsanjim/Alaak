8fc3a03a6529b5bc6e0d95bb7db7f3e73fbc400a
aleveyg <alejandra.gv@maanyuba.com>
2024-01-22 11:04:22 -0600

    filtro

--- src/component/BottomSheets/index.tsx
+++ src/component/BottomSheets/index.tsx
@@ -1,139 +1,136 @@
-import React, { useEffect } from "react";
-import { Text, View, Alert} from "react-native";
-import styles from "../Styles/BottomSheet";
-import Input from "../AleBoxView/input";
-import SearchIcon from "../Icons/SearchIcon";
-import ArrowIcon from "../Icons/ArrowIcon";
-import CheckBox from "@react-native-community/checkbox";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
-
+import React, {useEffect} from 'react';
+import {Text, View, Alert} from 'react-native';
+import styles from '../Styles/BottomSheet';
+import Input from '../AleBoxView/input';
+import SearchIcon from '../Icons/SearchIcon';
+import ArrowIcon from '../Icons/ArrowIcon';
+import CheckBox from '@react-native-community/checkbox';
+import {useState} from 'react';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const areas = [
-    {
-        id:1,
-        name:"Recursos Humanos",
-        labelKey:"RH" 
-    },
-    {
-        id:2,
-        name:"Diseño",
-        labelKey:"UX/UI" 
-    },
-    {
-        id:3,
-        name:"Finanzas",
-        labelKey:"FN" 
-    },
-    {
-        id:4,
-        name:"Desarrollo",
-        labelKey:"DEV" 
-    },
-]
+  {
+    id: 1,
+    name: 'Recursos Humanos',
+    labelKey: 'RH',
+  },
+  {
+    id: 2,
+    name: 'Diseño',
+    labelKey: 'UX/UI',
+  },
+  {
+    id: 3,
+    name: 'Finanzas',
+    labelKey: 'FN',
+  },
+  {
+    id: 4,
+    name: 'Desarrollo',
+    labelKey: 'DEV',
+  },
+];
 
-function FilterSheet( {onClose,updateFilter} ){
-    const [rememberPass, setRememberPass] = useState(false)
-    const [searchText,setSearchText] = useState('')
-    const [listFilter, setListFilter] = useState([]) 
-    
+function FilterSheet({onClose, updateFilter}) {
+  const [rememberPass, setRememberPass] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [listFilter, setListFilter] = useState([]);
 
-
-    const onAddFilter = (area:any) =>{
-        const validIndex = listFilter.findIndex((itemF)=> itemF.id === area.id)
-        if(validIndex > 0){
-            setListFilter(prevState => prevState.filter(item=> item.id != area.id) )
-            /*
+  const onAddFilter = (area: any) => {
+    const validIndex = listFilter.findIndex(itemF => itemF.id === area.id);
+    if (validIndex > 0) {
+      setListFilter(prevState => prevState.filter(item => item.id != area.id));
+      /*
             const newArray = [...listFilter]
             newArray.splice(validIndex,0)
             setListFilter(newArray)
             */
-        }else{
-            setListFilter(prevState => [...prevState,area])
-/*
+    } else {
+      setListFilter(prevState => [...prevState, area]);
+      /*
             const newArray = [...listFilter]
             newArray.push(area)
             setListFilter(newArray)
             */
-        }
-
     }
-//useEffect "cuando un elemento se actualiza" "Cuando el componente se va a morir"
-useEffect(()=>{
-    updateFilter(listFilter)
-},[listFilter.length])
-//props digging
-const onReset = ()=>{
-        setListFilter([])
-        setSearchText('')
-        onClose?.()
-}
+  };
+  //useEffect "cuando un elemento se actualiza" "Cuando el componente se va a morir"
+  useEffect(() => {
+    updateFilter(listFilter);
+  }, [listFilter.length]);
+  //props digging
+  const onReset = () => {
+    setListFilter([]);
+    setSearchText('');
+    onClose?.();
+  };
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-            <View style={{alignItems:"center", flexDirection:"row", justifyContent:"space-between"}}>
-            <TouchableOpacity onPress={onReset}>
-                        <ArrowIcon/>        
-                    </TouchableOpacity>
-                <Text style={styles.text}>Todos</Text>
-            </View>
-            </View>
-            <View style={styles.main}>
-                <View style={styles.completeFilter}>
-              
-                    <Text style={styles.textOne}>Filtrar por:</Text>
-                    <TouchableOpacity onPress={()=>setListFilter([]) } style={styles.cleanFilter}>
-                        <Text style={styles.textClean}>Limpiar filtro</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.search}>
-                    <SearchIcon/>
-                    <Input 
-                    onChangeText={(text)=> setSearchText(text)}
-                    style={{marginLeft:12}}
-                    placeholderTextColor={"#333"}
-                    placeholder="Recursos"
-                    value={searchText}/>
-        
-                </View>
-                <View style={{paddingHorizontal:24, paddingVertical:10}}>
-                    <Text style={styles.textTwo}>Área</Text>
-                </View>
-                {/*content Filter */}
-                <View>
-                    {/* item filter*/}
-                    {
-                        areas.map((area)=>(
-                            <View style={styles.departments} key={area.id}>
-                        <Text style={styles.textTwo}>{area.name}</Text>
-                        <CheckBox 
-                            value={ listFilter.find((item)=> item.id === area.id) ? true : false}
-                            onValueChange={event => {
-                               onAddFilter(area)
-                            }}
-                           />
-                    </View>
-                        ))
-                    }
-                </View>
-                <View style={styles.bottomFilter}>
-                    <TouchableOpacity style={styles.setFilter} 
-                    onPress={onReset}>
-                        <Text style={styles.textClean}>Cancelar</Text>
-                    </TouchableOpacity>                    
-                    <View style={styles.setFilterTwo}>
-                    <TouchableOpacity style={styles.setFilter} 
-                     onPress={onClose}>
-                        <Text style={styles.textCleanTwo}>Filtrar</Text>
-                        </TouchableOpacity>  
-                    </View>
-                </View>
-            </View>
-
-            
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View
+          style={{
+            alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <TouchableOpacity onPress={onReset}>
+            <ArrowIcon />
+          </TouchableOpacity>
+          <Text style={styles.text}>Todos</Text>
         </View>
-        
-    );
-};
+      </View>
+      <View style={styles.main}>
+        <View style={styles.completeFilter}>
+          <Text style={styles.textOne}>Filtrar por:</Text>
+          <TouchableOpacity
+            onPress={() => setListFilter([])}
+            style={styles.cleanFilter}>
+            <Text style={styles.textClean}>Limpiar filtro</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.search}>
+          <SearchIcon />
+          <Input
+            onChangeText={text => setSearchText(text)}
+            style={{marginLeft: 12}}
+            placeholderTextColor={'#333'}
+            placeholder="Recursos"
+            value={searchText}
+          />
+        </View>
+        <View style={{paddingHorizontal: 24, paddingVertical: 10}}>
+          <Text style={styles.textTwo}>Área</Text>
+        </View>
+        {/*content Filter */}
+        <View>
+          {/* item filter*/}
+          {areas.map(area => (
+            <View style={styles.departments} key={area.id}>
+              <Text style={styles.textTwo}>{area.name}</Text>
+              <CheckBox
+                value={
+                  listFilter.find(item => item.id === area.id) ? true : false
+                }
+                onValueChange={event => {
+                  onAddFilter(area);
+                }}
+              />
+            </View>
+          ))}
+        </View>
+        <View style={styles.bottomFilter}>
+          <TouchableOpacity style={styles.setFilter} onPress={onReset}>
+            <Text style={styles.textClean}>Cancelar</Text>
+          </TouchableOpacity>
+          <View style={styles.setFilterTwo}>
+            <TouchableOpacity style={styles.setFilter} onPress={onClose}>
+              <Text style={styles.textCleanTwo}>Filtrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
 export default FilterSheet;
--- src/scenes/organization/index.tsx
+++ src/scenes/organization/index.tsx
@@ -1,228 +1,297 @@
-import React, {useMemo,useRef,useState,useInsertionEffect, useEffect} from "react";
-import { SafeAreaView, View, Text, Image, StyleSheet, ScrollView, Button, Alert, TouchableOpacity, Touchable} from "react-native";
-import Input from "../../component/AleBoxView/input";
-import Box from "../../component/AleBoxView";
-import styles from "../../component/Styles/BowViewStyles";
-import FilterIcon from "../../component/Icons/FilterIcon";
-import NotificationIcon from "../../component/Icons/NotificationIcon";
-import SearchIcon from "../../component/Icons/SearchIcon";
-import ArrowIcon from "../../component/Icons/ArrowIcon";
-import SettingIcon from "../../component/Icons/SettingIcon";
-import { useNavigation } from "@react-navigation/native";
-import BottomSheet, {BottomSheetBackdrop} from "@gorhom/bottom-sheet";
-import FilterSheet from "../../component/BottomSheets";
+import React, {
+  useMemo,
+  useRef,
+  useState,
+  useInsertionEffect,
+  useEffect,
+} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Button,
+  Alert,
+  TouchableOpacity,
+  Touchable,
+} from 'react-native';
+import Input from '../../component/AleBoxView/input';
+import Box from '../../component/AleBoxView';
+import styles from '../../component/Styles/BowViewStyles';
+import FilterIcon from '../../component/Icons/FilterIcon';
+import NotificationIcon from '../../component/Icons/NotificationIcon';
+import SearchIcon from '../../component/Icons/SearchIcon';
+import ArrowIcon from '../../component/Icons/ArrowIcon';
+import SettingIcon from '../../component/Icons/SettingIcon';
+import {useNavigation} from '@react-navigation/native';
+import BottomSheet, {BottomSheetBackdrop} from '@gorhom/bottom-sheet';
+import FilterSheet from '../../component/BottomSheets';
 
 const team = [
-    {
-        avatar:require("../../component/Images/Avatar-base(6).png"),
-        firstName:"Amaury ",
-        lastName:"Almanza",
-        area:"CEO ",
-        email: "amaury@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(7).png"),
-        firstName:"Emilio ",
-        lastName:"Camargo",
-        area:"CTO ",
-        email: "emilio@maanyuba.com" 
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(8).png"),
-        firstName:"César ",
-        lastName:"Mendoza",
-        area:"VP ",
-        email: "cesar@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(9).png"),
-        firstName:"Claudia ",
-        lastName:"Rodríguez",
-        area:"Product",
-        email: "claudia@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(10).png"),
-        firstName:"Juan",
-        lastName:"Rivera",
-        area:"App MovileI",
-        email: "juan.ro@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(5).png"),
-        firstName:"Humberto ",
-        lastName:"Alonso",
-        area:"UX&UI Manager",
-        email: "humberto.am@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(4).png"),
-        firstName:"Luis ",
-        lastName:"Mota",
-        area:"Trainne UX&UI ",
-        email: "luis.mg@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(2).png"),
-        firstName:"César",
-        lastName:"Ramón",
-        area:"Trainne UX&UI ",
-        email: "maki@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(1).png"),
-        firstName:"Aline",
-        lastName:"Palacios",
-        area:"Trainne UX&UI ",
-        email: "aline.pd@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images/Avatar-base(3).png"),
-        firstName:"Nathali",
-        lastName:"Ortiz",
-        area:"Trainne UX&UI ",
-        email: "emilio@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images//Avatar-base.png"),
-        firstName:"Vanessa",
-        lastName:"Luna",
-        area:"Trainne UX&UI ",
-        email: "vanessa.lc@maanyuba.com"
-    },
-    {
-        avatar:require("../../component/Images//Avatar-base.png"),
-        firstName:"Dylan",
-        lastName:"Torres",
-        area:"Developer",
-        email: "dylan.tc@maanyuba.com"
-    }
-]
+  {
+    avatar: require('../../component/Images/Avatar-base(6).png'),
+    firstName: 'Amaury ',
+    lastName: 'Almanza',
+    area: 'CEO ',
+    email: 'amaury@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(7).png'),
+    firstName: 'Emilio ',
+    lastName: 'Camargo',
+    area: 'CTO ',
+    email: 'emilio@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(8).png'),
+    firstName: 'César ',
+    lastName: 'Mendoza',
+    area: 'VP ',
+    email: 'cesar@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(9).png'),
+    firstName: 'Claudia ',
+    lastName: 'Rodríguez',
+    area: 'Product',
+    email: 'claudia@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(10).png'),
+    firstName: 'Juan',
+    lastName: 'Rivera',
+    area: 'App MovileI',
+    email: 'juan.ro@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(5).png'),
+    firstName: 'Humberto ',
+    lastName: 'Alonso',
+    area: 'UX&UI Manager',
+    email: 'humberto.am@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(4).png'),
+    firstName: 'Luis ',
+    lastName: 'Mota',
+    area: 'Trainne UX&UI ',
+    email: 'luis.mg@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(2).png'),
+    firstName: 'César',
+    lastName: 'Ramón',
+    area: 'Trainne UX&UI ',
+    email: 'maki@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(1).png'),
+    firstName: 'Aline',
+    lastName: 'Palacios',
+    area: 'Trainne UX&UI ',
+    email: 'aline.pd@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images/Avatar-base(3).png'),
+    firstName: 'Nathali',
+    lastName: 'Ortiz',
+    area: 'Trainne UX&UI ',
+    email: 'emilio@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images//Avatar-base.png'),
+    firstName: 'Vanessa',
+    lastName: 'Luna',
+    area: 'Trainne UX&UI ',
+    email: 'vanessa.lc@maanyuba.com',
+  },
+  {
+    avatar: require('../../component/Images//Avatar-base.png'),
+    firstName: 'Dylan',
+    lastName: 'Torres',
+    area: 'Developer',
+    email: 'dylan.tc@maanyuba.com',
+  },
+];
 
-function OrganScene(){
-  const navigation = useNavigation()
+function OrganScene() {
+  const navigation = useNavigation();
   //ref
   const bottomSheetRef = useRef<BottomSheet>(null);
- // variables
- const snapPoints = useMemo(() => ['25%', '93%'], []);
- //hook
- const [searchText,setSearchText] = useState('')
- const [buttonActive,setButtonActive] = useState("all") // "all" Vert todo - "mine" Mi equipo
- const [teamFilter,setTeamFilter] =useState ([])
- const [chips,setChips] = useState([]);
- const myArea = "UX&UI";
- //console.log("search",team.filter((item)=> item.firstName.toLowerCase().includes(searchText.toLowerCase()) ))
-  
- useEffect(()=>{
-    const arrayFilter = team.filter((item)=> buttonActive === 'all' ? item : item.area.includes(myArea) )
-    const newTeam = arrayFilter.filter((item)=> item.firstName.toLowerCase().includes(searchText.toLowerCase()) || item.lastName.toLowerCase().includes(searchText.toLowerCase()) )
-    setTeamFilter(newTeam)
-},[buttonActive,searchText] )
- return (
-      
-       <SafeAreaView style={styles.container}>
-          <View style={styles.header}>
-              <View style={{alignItems:"flex-end", flexDirection:"row", justifyContent:"space-between"}}>
-                   <ArrowIcon/>
-                   <Text style={styles.text}> Perfil Empresarial </Text>
-                   <View style={{flexDirection:"row"}}>
-                       <NotificationIcon/>
-                       <SettingIcon/>
-                   </View>
-              </View>
-          </View>
-          {/*SearchBar*/}
-          <View style={styles.buttonSearch}>
-               <View style={{
-                   flexDirection:"row",
-                   backgroundColor: "#fff",
-                   borderRadius:100,
-                   height:56,
-                   borderWidth:0.5,
-                   borderColor:"#E8E8E9",
-                   paddingHorizontal:24,
-                   alignItems:"center"
-               }}>
-                   <SearchIcon/>
-                   <Input 
-                   onChangeText={(text)=> setSearchText(text)}
-                   style={{marginLeft:12}}
-                   placeholderTextColor={"#333"}
-                   placeholder="Buscar en Mi empresa"/>
-               </View>
-              
-          </View>
-          {/*Buttons Filter*/}
-          <View style={styles.filter}>
-              <View style={styles.buttonShape}><Text style={styles.textFil}>Filtros</Text></View>   
-              <TouchableOpacity onPress={()=>bottomSheetRef.current?.expand(BottomSheet)} style={styles.buttonShapeFour}>
-                <FilterIcon />
-              </TouchableOpacity>
-              <TouchableOpacity 
-              onPress={()=> setButtonActive("mine")}
-              style={[ buttonActive === "mine" ? styles.buttonShapeThree:styles.buttonShapeTwo]}>
-                <Text style={[buttonActive === "mine" ? styles.textFilThree: styles.textFiltwo]}>Mi equipo</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={()=> setButtonActive("all")}>
-                <View style={[ buttonActive === "all" ? styles.buttonShapeThree:styles.buttonShapeTwo]}>
-                  <Text style={[buttonActive === "all" ? styles.textFilThree: styles.textFiltwo]}>Todos</Text>
-                </View>
-              </TouchableOpacity> 
-          </View>
+  // variables
+  const snapPoints = useMemo(() => ['25%', '93%'], []);
+  //hook
+  const [searchText, setSearchText] = useState('');
+  const [buttonActive, setButtonActive] = useState('all'); // "all" Vert todo - "mine" Mi equipo
+  const [teamFilter, setTeamFilter] = useState([]);
+  const [chips, setChips] = useState([]);
+  const myArea = 'UX&UI';
+  //console.log("search",team.filter((item)=> item.firstName.toLowerCase().includes(searchText.toLowerCase()) ))
 
-         {/*Filtros by bottomSheet*/}
-         <View style={styles.fil}>
-         <View style={{flexDirection:'row',flexWrap:"wrap"}}>
-           {chips.map((chip)=> (
-            <View key={chip.id} style={{flex:1,width:95,height:33,borderRadius:100,borderColor:"#FFF",
-            borderWidth:1,backgroundColor: "#FFF",marginLeft:12,justifyContent:"center",alignItems:"center"}}>
-            <Text style={{color: "#333333",textAlign: "center",fontFamily: "Poppins",fontSize: 14,fontStyle: "normal",
-            fontWeight: "500",height: 21,width: 104,}}>{chip.labelKey}</Text>  
-            
-            </View>
-            
-           ))}    
-        </View>  
+  useEffect(() => {
+    const arrayFilter = team.filter(item =>
+      buttonActive === 'all' ? item : item.area.includes(myArea),
+    );
+    const newTeam = arrayFilter.filter(
+      item =>
+        item.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.lastName.toLowerCase().includes(searchText.toLowerCase()),
+    );
+    setTeamFilter(newTeam);
+  }, [buttonActive, searchText]);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View
+          style={{
+            alignItems: 'flex-end',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <ArrowIcon />
+          <Text style={styles.text}> Perfil Empresarial </Text>
+          <View style={{flexDirection: 'row'}}>
+            <NotificationIcon />
+            <SettingIcon />
+          </View>
         </View>
-         {/*Content*/}
-          <View style={styles.main}>
-          <ScrollView>
-              <Text style={styles.texthree}> Resultado de tu búsqueda </Text>
-              <Box data={
-                teamFilter
-              }/>
-          </ScrollView>
+      </View>
+      {/*SearchBar*/}
+      <View style={styles.buttonSearch}>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#fff',
+            borderRadius: 100,
+            height: 56,
+            borderWidth: 0.5,
+            borderColor: '#E8E8E9',
+            paddingHorizontal: 24,
+            alignItems: 'center',
+          }}>
+          <SearchIcon />
+          <Input
+            onChangeText={text => setSearchText(text)}
+            style={{marginLeft: 12}}
+            placeholderTextColor={'#333'}
+            placeholder="Buscar en Mi empresa"
+          />
+        </View>
+      </View>
+      {/*Buttons Filter*/}
+      <View style={styles.filter}>
+        <View style={styles.buttonShape}>
+          <Text style={styles.textFil}>Filtros</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => bottomSheetRef.current?.expand(BottomSheet)}
+          style={styles.buttonShapeFour}>
+          <FilterIcon />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setButtonActive('mine')}
+          style={[
+            buttonActive === 'mine'
+              ? styles.buttonShapeThree
+              : styles.buttonShapeTwo,
+          ]}>
+          <Text
+            style={[
+              buttonActive === 'mine' ? styles.textFilThree : styles.textFiltwo,
+            ]}>
+            Mi equipo
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setButtonActive('all')}>
+          <View
+            style={[
+              buttonActive === 'all'
+                ? styles.buttonShapeThree
+                : styles.buttonShapeTwo,
+            ]}>
+            <Text
+              style={[
+                buttonActive === 'all'
+                  ? styles.textFilThree
+                  : styles.textFiltwo,
+              ]}>
+              Todos
+            </Text>
           </View>
-       
+        </TouchableOpacity>
+      </View>
 
-       <BottomSheet
-           ref={bottomSheetRef}
-           index={-1}
-           snapPoints={snapPoints}
-           enableOverDrag
-           enablePanDownToClose={true}
-           backgroundStyle={{backgroundColor:"#fff"}}
-           backdropComponent={(props) => (
-               <BottomSheetBackdrop
-               {...props}
-               opacity={0.5}
-               enableTouchThrough={false}
-               appearsOnIndex={0}
-               disappearsOnIndex={-1}
-               style={[{backgroundColor:'rgba(0, 0, 0, 1)'}, StyleSheet.absoluteFillObject]}
-               />
-           )}
-       >
-           <ScrollView>
-               <FilterSheet onClose={()=>bottomSheetRef.current?.close()} updateFilter={setChips}/>
-           </ScrollView>
-           </BottomSheet>
-           
-       </SafeAreaView>
+      {/*Filtros by bottomSheet*/}
+{chips.length?<View style={styles.fil}>
+        <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+          {chips.map(chip => (
+            <View
+              key={chip.id}
+              style={{
+                flex: 1,
+                width: 95,
+                height: 33,
+                borderRadius: 100,
+                borderColor: '#FFF',
+                borderWidth: 1,
+                backgroundColor: '#FFF',
+                marginLeft: 12,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  color: '#333333',
+                  textAlign: 'center',
+                  fontFamily: 'Poppins',
+                  fontSize: 14,
+                  fontStyle: 'normal',
+                  fontWeight: '500',
+                  height: 21,
+                  width: 104,
+                }}>
+                {chip.labelKey}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>: null}
 
+      {/*Content*/}
+      <View style={styles.main}>
+          <Box data={teamFilter} />
+      </View>
 
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        enableOverDrag
+        enablePanDownToClose={true}
+        backgroundStyle={{backgroundColor: '#fff'}}
+        backdropComponent={props => (
+          <BottomSheetBackdrop
+            {...props}
+            opacity={0.5}
+            enableTouchThrough={false}
+            appearsOnIndex={0}
+            disappearsOnIndex={-1}
+            style={[
+              {backgroundColor: 'rgba(0, 0, 0, 1)'},
+              StyleSheet.absoluteFillObject,
+            ]}
+          />
+        )}>
+        <ScrollView>
+          <FilterSheet
+            onClose={() => bottomSheetRef.current?.close()}
+            updateFilter={setChips}
+          />
+        </ScrollView>
+      </BottomSheet>
+    </SafeAreaView>
   );
-
-
 }
 export default OrganScene;
